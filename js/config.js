8733959f7827e619e9b7a9d163103e4a700c2357
pabloarca <31969557+pabloarca@users.noreply.github.com>
2018-04-09 17:45:26 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,14 +1,14 @@
 //all the provided keys are examples, go to Amazon Cognito and get yours
 
-AWSCognito.config.region = 'eu-west-1'; //This is required to derive the endpoint
+AWSCognito.config.region = 'eu-central-1'; //This is required to derive the endpoint
 
 var poolData = {
-    UserPoolId : 'eu-west-1_9jknknuuu7i97', // your user pool id here
-    ClientId : '9832knksndh382u3ukjkjiuiu' // your client id here
+    UserPoolId : 'eu-central-1_RJgUcRNLj', // your user pool id here
+    ClientId : '7scaq5pcjfe390t9fosolpgl1c' // your client id here
 };
 
-var identityPoolId = 'eu-west-1:928sjpf-283osj3-293us3js-82372-730s'; //go to AWS Cognito Federated Identites
+var identityPoolId = 'eu-central-1:ed98c61f-23a0-4fbf-9f6e-1f1541bb9a0b'; //go to AWS Cognito Federated Identites
 
-var userAttributes = ['email', 'phone_number']; //the standard attributes you require in AWS Cognito
+var userAttributes = ['email', '']; //the standard attributes you require in AWS Cognito
 
 var MFARequired = true; //do you require your clients to use MFA?
