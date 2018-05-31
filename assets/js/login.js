50e3808fee6d0ccecf4a72195978469ffe4a9b8a
pabloarca <pablo_mlg22@hotmail.com>
2018-05-31 18:39:49 +0200

    1

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,9 +1,58 @@
-'use strict'
+(function() {
 
-let login = document.getElementById('login'),
-    close = document.getElementById('login__close'),
-    control = document.getElementById('login__control');
+//Inicializar Firebase
+  const config = {
+    apiKey: "AIzaSyBl5JlfDaN6JhPgHZhYNlP3OURPpgzj_2U",
+    authDomain: "recomendador-42ed0.firebaseapp.com",
+    databaseURL: "https://recomendador-42ed0.firebaseio.com",
+    projectId: "recomendador-42ed0",
+    storageBucket: "",
+    messagingSenderId: "384548144541"
+  };
+  firebase.initializeApp(config);
 
-close.addEventListener('click', () => login.classList.toggle('login--visible'));
-control.addEventListener('click', () => login.classList.toggle('login--visible'));
-login.addEventListener('submit', () => login.classList.toggle('login--visible'));
+  // Obtener elementos
+  const txtEmail = document.getElementById('txtEmail');
+  const txtPassword = document.getElementById('txtPassword');
+  const btnLogin = document.getElementById('btnLogin');
+  const btnSignUp = document.getElementById('btnSignUp');
+  const btnLogout = document.getElementById('btnLogout');
+
+  // Añadir Evento login
+  btnLogin.addEventListener('click', e => {
+    //Obtener email y pass
+    const email = txtEmail.value;
+    const pass = txtPassword.value;
+    const auth = firebase.auth();
+    // Sign in
+    const promise = auth.signInWithEmailAndPassword(email, pass);
+    promise.catch(e => console.log(e.message));
+  });
+
+  // Añadir evento signup
+  btnSignUp.addEventListener('click', e => {
+    // Obtener email y pass
+    // TODO: comprobar que el email sea real
+    const email = txtEmail.value;
+    const pass = txtPassword.value;
+    const auth = firebase.auth();
+    // Sign in
+    const promise = auth.createUserWithEmailAndPassword(email, pass);
+    promise.catch(e => console.log(e.message));
+  });
+
+  btnLogout.addEventListener('click', e => {
+    firebase.auth().signOut();
+  });
+
+  // Añadir un listener en tiempo real
+   firebase.auth().onAuthStateChanged( firebaseUser => {
+    if(firebaseUser) {
+      console.log(firebaseUser);
+      btnLogout.classList.remove('hide');
+    } else {
+      console.log('no logueado');
+      btnLogout.classList.add('hide');
+    }
+  });
+} ());
