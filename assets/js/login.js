6cde603c00dab6023ac5ba7e45d3f2543a218ec9
pabloarca <pablo_mlg22@hotmail.com>
2018-06-06 21:25:14 +0200

    2

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,58 +1,63 @@
-(function() {
+function registrar(){
+  var email = document.getElementById('email').value;
+  var password = document.getElementById('password').value;
 
-//Inicializar Firebase
-  const config = {
-    apiKey: "AIzaSyBl5JlfDaN6JhPgHZhYNlP3OURPpgzj_2U",
-    authDomain: "recomendador-42ed0.firebaseapp.com",
-    databaseURL: "https://recomendador-42ed0.firebaseio.com",
-    projectId: "recomendador-42ed0",
-    storageBucket: "",
-    messagingSenderId: "384548144541"
-  };
-  firebase.initializeApp(config);
-
-  // Obtener elementos
-  const txtEmail = document.getElementById('txtEmail');
-  const txtPassword = document.getElementById('txtPassword');
-  const btnLogin = document.getElementById('btnLogin');
-  const btnSignUp = document.getElementById('btnSignUp');
-  const btnLogout = document.getElementById('btnLogout');
-
-  // Añadir Evento login
-  btnLogin.addEventListener('click', e => {
-    //Obtener email y pass
-    const email = txtEmail.value;
-    const pass = txtPassword.value;
-    const auth = firebase.auth();
-    // Sign in
-    const promise = auth.signInWithEmailAndPassword(email, pass);
-    promise.catch(e => console.log(e.message));
+  firebase.auth().createUserWithEmailAndPassword(email, password)
+  .catch(function(error){
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    console.log(errorCode);
+    console.log(errorMessage);
   });
+}
 
-  // Añadir evento signup
-  btnSignUp.addEventListener('click', e => {
-    // Obtener email y pass
-    // TODO: comprobar que el email sea real
-    const email = txtEmail.value;
-    const pass = txtPassword.value;
-    const auth = firebase.auth();
-    // Sign in
-    const promise = auth.createUserWithEmailAndPassword(email, pass);
-    promise.catch(e => console.log(e.message));
-  });
 
-  btnLogout.addEventListener('click', e => {
-    firebase.auth().signOut();
-  });
+function ingreso(){
 
-  // Añadir un listener en tiempo real
-   firebase.auth().onAuthStateChanged( firebaseUser => {
-    if(firebaseUser) {
-      console.log(firebaseUser);
-      btnLogout.classList.remove('hide');
-    } else {
-      console.log('no logueado');
-      btnLogout.classList.add('hide');
-    }
-  });
-} ());
+  var email2 = document.getElementById('email2').value;
+  var contrasena = document.getElementById('contrasena').value;
+
+
+  firebase.auth().createUserWithEmailAndPassword(email2, contrasena)
+
+  .catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    console.log(errorCode);
+    console.log(errorMessage);
+});
+
+}
+
+
+function observador(){
+  firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+      console.log('existe usuario activo')
+      aparece();
+    // User is signed in.
+    var displayName = user.displayName;
+    var email = user.email;
+    var emailVerified = user.emailVerified;
+    var photoURL = user.photoURL;
+    var isAnonymous = user.isAnonymous;
+    var uid = user.uid;
+    var providerData = user.providerData;
+    // ...
+  } else {
+    // User is signed out.
+    console.log('no existe usuario activo')
+    // ...
+  }
+});
+}
+
+//ejecutamos autmanticamente el observador
+observador();
+
+function aparece(){
+  var contenido = document.getElementById('contenido');
+  contenido.innerHTML = "solo lo ve usuario activo";
+
+}
